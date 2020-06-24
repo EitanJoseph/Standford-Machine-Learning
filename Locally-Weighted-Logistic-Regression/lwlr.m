67e2b37c63d27fa8c662abfdffb0d5bb35f71f3f
eitanjoseph <eitan17j@gmail.com>
2020-06-23 21:05:46 -0400

    clean .m~

--- Locally-Weighted-Logistic-Regression/lwlr.m
+++ Locally-Weighted-Logistic-Regression/lwlr.m
@@ -1,5 +1,10 @@
+%%Caroline Wang
+%%Eitan Joseph
+
 function y = lwlr(X_train, y_train, x, tau)
+%Storing feature matrix size information
 [m,n]=size(X_train);
+%Initializing parameter vector
 Theta = zeros(n,1);
 
 % Building W
@@ -17,12 +22,14 @@ while(grad_theta > 1e-6*ones(n,1))
     grad_theta = transpose(X_train)*Z - lambda*Theta;
     %Building Hessian
     d = -W.*h.*(1-h);
+    % We can construct the diagnol matrix of size mxm using diag(d)
     D = diag(d);
     Hessian = transpose(X_train)*D*X_train - lambda*eye(n,n);
     %Updating Theta
     Theta = Theta - inv(Hessian)*grad_theta;
 end
 
+%The final hypothesis evaluation for prediction
 newH = 1./(1.+exp(-transpose(Theta)*x));
 y = newH > 0.5;
 
